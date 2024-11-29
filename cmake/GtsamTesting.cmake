4eff17835248c62e2cd1333bfd6f8b06171989d8
Jai Moraes <jmoraes@blackberry.com>
2024-11-29 16:00:18 -0500

    ADDED: Test Installation when building with QNX (Review this for upstreaming)

--- cmake/GtsamTesting.cmake
+++ cmake/GtsamTesting.cmake
@@ -196,8 +196,13 @@ macro(gtsamAddTestsGlob_impl groupName globPatterns excludedFiles linkLibraries)
 				# Add TOPSRCDIR
 				set_property(SOURCE ${script_src} APPEND PROPERTY COMPILE_DEFINITIONS "TOPSRCDIR=\"${GTSAM_SOURCE_DIR}\"")
 
+				if(QNX)
+				# Do not exclude from 'make all' and 'make install'
+				set_target_properties(${target_name} PROPERTIES EXCLUDE_FROM_ALL OFF)
+				else()
 				# Exclude from 'make all' and 'make install'
-				set_target_properties(${script_name} PROPERTIES EXCLUDE_FROM_ALL ON)
+				set_target_properties(${target_name} PROPERTIES EXCLUDE_FROM_ALL ON)
+				endif()
 
 				# Configure target folder (for MSVC and Xcode)
 				set_property(TARGET ${script_name} PROPERTY FOLDER "Unit tests/${groupName}")
