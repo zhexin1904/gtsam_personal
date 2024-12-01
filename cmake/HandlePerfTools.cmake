486feeb385345d13698e67a0ff4f20161ad8de57
Varun Agrawal <varagrawal@gmail.com>
2024-12-01 18:58:06 -0500

    link gperftools libraries

--- cmake/HandlePerfTools.cmake
+++ cmake/HandlePerfTools.cmake
@@ -2,3 +2,4 @@
 ###############################################################################
 # Find Google perftools
 find_package(GooglePerfTools)
+target_link_libraries(${PROJECT_NAME} ${GPERFTOOLS_TCMALLOC} ${GPERFTOOLS_PROFILER})
