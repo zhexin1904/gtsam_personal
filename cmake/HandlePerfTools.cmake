3980a439c0fc655ac73a2763f60109b5e6a9270f
Varun Agrawal <varagrawal@gmail.com>
2024-12-02 09:53:33 -0500

    link gperftools in the top CMakeLists.txt

--- cmake/HandlePerfTools.cmake
+++ cmake/HandlePerfTools.cmake
@@ -2,4 +2,3 @@
 ###############################################################################
 # Find Google perftools
 find_package(GooglePerfTools)
-target_link_libraries(${PROJECT_NAME} ${GPERFTOOLS_TCMALLOC} ${GPERFTOOLS_PROFILER})
