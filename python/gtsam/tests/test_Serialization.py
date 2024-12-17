60b00ebdda6da38bf26c35d112cee6d964bd5da8
Varun Agrawal <varagrawal@gmail.com>
2024-12-17 10:08:19 -0500

    improve testing for deep copy

--- python/gtsam/tests/test_Serialization.py
+++ python/gtsam/tests/test_Serialization.py
@@ -5,11 +5,11 @@ All Rights Reserved
 
 See LICENSE for the license information
 
-KalmanFilter unit tests.
-Author: Frank Dellaert & Duy Nguyen Ta (Python)
+Serialization and deep copy tests.
+
+Author: Varun Agrawal
 """
 import unittest
-from copy import deepcopy
 
 import numpy as np
 from gtsam.symbol_shorthand import B, V, X
@@ -18,42 +18,36 @@ from gtsam.utils.test_case import GtsamTestCase
 import gtsam
 
 
-class TestSerialization(GtsamTestCase):
-    """Tests for serialization of various GTSAM objects."""
+class TestDeepCopy(GtsamTestCase):
+    """Tests for deep copy of various GTSAM objects."""
 
     def test_PreintegratedImuMeasurements(self):
         """
-        Test the serialization of `PreintegratedImuMeasurements` by performing a deepcopy.
+        Test the deep copy of `PreintegratedImuMeasurements` by performing a deepcopy.
         """
         params = gtsam.PreintegrationParams(np.asarray([0, 0, -9.81]))
         pim = gtsam.PreintegratedImuMeasurements(params)
 
-        # If serialization failed, then this will throw an error
-        pim2 = deepcopy(pim)
-        self.assertEqual(pim, pim2)
+        self.assertDeepCopyEquality(pim)
 
     def test_ImuFactor(self):
         """
-        Test the serialization of `ImuFactor` by performing a deepcopy.
+        Test the deep copy of `ImuFactor` by performing a deepcopy.
         """
         params = gtsam.PreintegrationParams(np.asarray([0, 0, -9.81]))
         pim = gtsam.PreintegratedImuMeasurements(params)
-        imu_odom = gtsam.ImuFactor(X(0), V(0), X(1), V(1), B(0), pim)
+        imu_factor = gtsam.ImuFactor(X(0), V(0), X(1), V(1), B(0), pim)
 
-        # If serialization failed, then this will throw an error
-        imu_odom2 = deepcopy(imu_odom)
-        self.assertEqual(imu_odom, imu_odom2)
+        self.assertDeepCopyEquality(imu_factor)
 
     def test_PreintegratedCombinedMeasurements(self):
         """
-        Test the serialization of `PreintegratedCombinedMeasurements` by performing a deepcopy.
+        Test the deep copy of `PreintegratedCombinedMeasurements` by performing a deepcopy.
         """
         params = gtsam.PreintegrationCombinedParams(np.asarray([0, 0, -9.81]))
         pim = gtsam.PreintegratedCombinedMeasurements(params)
 
-        # If serialization failed, then this will throw an error
-        pim2 = deepcopy(pim)
-        self.assertEqual(pim, pim2)
+        self.assertDeepCopyEquality(pim)
 
 
 if __name__ == "__main__":
