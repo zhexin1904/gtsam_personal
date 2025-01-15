94e96ab001a682657e04f7e2524d67335fcb2421
morten <morten.c.nissov@gmail.com>
2025-01-15 13:38:08 +0100

    remove EstimateState from GPSFactorArm

--- gtsam/navigation/GPSFactor.h
+++ gtsam/navigation/GPSFactor.h
@@ -176,14 +176,6 @@ public:
     return bL_;
   }
 
-  /**
-   *  Convenience function to estimate state at time t, given two GPS
-   *  readings (in local NED Cartesian frame) bracketing t
-   *  Assumes roll is zero, calculates yaw and pitch from NED1->NED2 vector.
-   */
-  static std::pair<Pose3, Vector3> EstimateState(double t1, const Point3& NED1,
-      double t2, const Point3& NED2, double timestamp);
-
 };
 
 /**
