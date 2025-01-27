35e7acbf16548a286d3677717f8dfc1b9398d1f8
Frank Dellaert <dellaert@gmail.com>
2025-01-27 13:05:20 -0500

    Print factors

--- gtsam/discrete/DiscreteJunctionTree.cpp
+++ gtsam/discrete/DiscreteJunctionTree.cpp
@@ -16,19 +16,42 @@
  * @author Richard Roberts
  */
 
-#include <gtsam/inference/JunctionTree-inst.h>
-#include <gtsam/discrete/DiscreteJunctionTree.h>
 #include <gtsam/discrete/DiscreteEliminationTree.h>
+#include <gtsam/discrete/DiscreteJunctionTree.h>
+#include <gtsam/inference/JunctionTree-inst.h>
 
 namespace gtsam {
 
-  // Instantiate base classes
-  template class EliminatableClusterTree<DiscreteBayesTree, DiscreteFactorGraph>;
-  template class JunctionTree<DiscreteBayesTree, DiscreteFactorGraph>;
+// Instantiate base classes
+template class EliminatableClusterTree<DiscreteBayesTree, DiscreteFactorGraph>;
+template class JunctionTree<DiscreteBayesTree, DiscreteFactorGraph>;
 
-  /* ************************************************************************* */
-  DiscreteJunctionTree::DiscreteJunctionTree(
-    const DiscreteEliminationTree& eliminationTree) :
-  Base(eliminationTree) {}
+/* ************************************************************************* */
+DiscreteJunctionTree::DiscreteJunctionTree(
+    const DiscreteEliminationTree& eliminationTree)
+    : Base(eliminationTree) {}
+/* ************************************************************************* */
+namespace {
+struct PrintForestVisitorPre {
+  const KeyFormatter& formatter;
+  PrintForestVisitorPre(const KeyFormatter& formatter) : formatter(formatter) {}
+  std::string operator()(
+      const std::shared_ptr<DiscreteJunctionTree::Cluster>& node,
+      const std::string& parentString) {
+    // Print the current node
+    node->print(parentString + "-", formatter);
+    node->factors.print(parentString + "-", formatter);
+    std::cout << std::endl;
+    // Increment the indentation
+    return parentString + "| ";
+  }
+};
+}  // namespace
 
+void DiscreteJunctionTree::print(const std::string& s,
+                                 const KeyFormatter& keyFormatter) const {
+  PrintForestVisitorPre visitor(keyFormatter);
+  treeTraversal::DepthFirstForest(*this, s, visitor);
 }
+
+}  // namespace gtsam
