3c67e1fc21c55582a817aa7b5f1605466023c620
Varun Agrawal <varagrawal@gmail.com>
2025-01-30 10:53:52 -0500

    improve plotting script for City10000

--- examples/plot_city10000.m
+++ examples/plot_city10000.m
@@ -6,7 +6,7 @@ gt = dlmread('Data/ISAM2_GT_city10000.txt');
 eh_poses = dlmread('../build/examples/ISAM2_city10000.txt');
 
 % Generate by running `make Hybrid_City10000.run`
-h_poses = dlmread('../build/examples/HybridISAM_city10000.txt');
+h_poses = dlmread('../build/examples/Hybrid_City10000.txt');
 
 % Plot the same number of GT poses as estimated ones
 % gt = gt(1:size(eh_poses, 1), :);
@@ -18,7 +18,7 @@ figure(1)
 hold on;
 axis equal;
 axis([-65 65 -75 60])
-title('City10000 result with Hybrid Factor Graphs');
+% title('City10000 result with Hybrid Factor Graphs');
 plot(gt(:,1), gt(:,2), '--', 'LineWidth', 4, 'color', [0.1 0.7 0.1 0.5]);
 plot(h_poses(:,1), h_poses(:,2), '-', 'LineWidth', 2, 'color', [0.1 0.1 0.9 0.4]);
 legend('Ground truth', 'Hybrid Factor Graphs');
@@ -28,7 +28,7 @@ figure(2)
 hold on;
 axis equal;
 axis([-65 65 -75 60])
-title('City10000 result with ISAM2');
+% title('City10000 result with ISAM2');
 plot(gt(:,1), gt(:,2), '--', 'LineWidth', 4, 'color', [0.1 0.7 0.1 0.5]);
 plot(eh_poses(:,1), eh_poses(:,2), '-', 'LineWidth', 2, 'color', [0.9 0.1 0. 0.4]);
 legend('Ground truth', 'ISAM2');
