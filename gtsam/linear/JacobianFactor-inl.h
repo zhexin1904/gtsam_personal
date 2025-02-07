d22546edad14840c3f9c8aaf045fe8ec4ab3fc9e
jmackay2 <1.732mackay@gmail.com>
2025-02-06 21:44:52 -0500

    guard warning supression with a gcc version

--- gtsam/linear/JacobianFactor-inl.h
+++ gtsam/linear/JacobianFactor-inl.h
@@ -20,7 +20,7 @@
 
 #include <gtsam/linear/linearExceptions.h>
 
-#if defined(__GNUC__) && !defined(__clang__)
+#if defined(__GNUC__) && !defined(__clang__) && __GNUC__ >= 13
 #pragma GCC diagnostic warning "-Wstringop-overread"
 #endif
 
