e6c1428357d095f637d50e7b4fb6c5702090fdfe
mkielo3 <mkielo3@gatech.edu>
2025-05-16 10:48:50 -0400

    cleanup

--- gtsam/navigation/LieGroupEKF.h
+++ gtsam/navigation/LieGroupEKF.h
@@ -26,9 +26,6 @@
 
 #include <gtsam/navigation/ManifoldEKF.h> // Include the base class
 #include <gtsam/base/Lie.h> // Include for Lie group traits and operations
-// #include "gtsam/base/Matrix.h"
-#include "gtsam/base/lieProxies.h"
-
 
 #include <Eigen/Dense>
 #include <type_traits>
